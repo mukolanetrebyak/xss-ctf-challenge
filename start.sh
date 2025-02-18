d52f633aac3d2096ad2acfd641ad8f4f7ebb59be
mukolanetrebyak <112856890+mukolanetrebyak@users.noreply.github.com>
2025-02-18 15:35:15 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source /home/mykola-cs/Документи/scripts/xss-ctf-challenge/env/bin/activate
-nohup python3 /home/mykola-cs/Документи/scripts/xss-ctf-challenge/wsgi.py >/dev/null 2>&1 &
+source env/bin/activate
+nohup python3 wsgi.py >/dev/null 2>&1 &
