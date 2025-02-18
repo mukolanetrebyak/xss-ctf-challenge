122b87b2a39d9b7970809aad8e1fa3e2d3d85aa2
mukolanetrebyak <112856890+mukolanetrebyak@users.noreply.github.com>
2025-02-18 14:47:20 +0200

    Update init_app.py

--- init_app.py
+++ init_app.py
@@ -10,9 +10,18 @@ from validators import escape
 def create_app():
     app = Flask(__name__)
     load_dotenv()
-    app.config["IP_OR_DOMAIN_WITH_PORT"] = "{}:{}".format(os.environ["IP_OR_DOMAIN"], os.environ["PORT"])
+    
+    ip_or_domain = os.environ.get("IP_OR_DOMAIN")
+    port = os.environ.get("PORT")
+    print(f"IP_OR_DOMAIN: {ip_or_domain}, PORT: {port}")  # Для перевірки значень
+    
+    app.config["IP_OR_DOMAIN_WITH_PORT"] = "{}:{}".format(ip_or_domain, port)
+    print(f"IP_OR_DOMAIN_WITH_PORT: {app.config['IP_OR_DOMAIN_WITH_PORT']}")  # Для перевірки результату
+    
     app.config["CAPTCHA_TOKEN"] = os.getenv("CAPTCHA_TOKEN")
     app.config["CAPTCHA_SITE_KEY"] = os.getenv("CAPTCHA_SITE_KEY")
+    
+    print(f"CAPTCHA_TOKEN: {app.config['CAPTCHA_TOKEN']}, CAPTCHA_SITE_KEY: {app.config['CAPTCHA_SITE_KEY']}")  # Для перевірки токенів
 
     from jobs import rq
     rq.init_app(app)
