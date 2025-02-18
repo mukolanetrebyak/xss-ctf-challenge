07607abe864f7f5a675880e68aa48fa6685c33a3
mukolanetrebyak <112856890+mukolanetrebyak@users.noreply.github.com>
2025-02-18 14:45:18 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
+import os
 from init_app import create_app
 
 if __name__ == '__main__':
-    create_app().run()
+    app = create_app()
+    app.run(host=os.environ.get("IP_OR_DOMAIN"), port=int(os.environ.get("PORT")))
